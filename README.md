3a3a9e128ce3b3d3f85734368f68544b529d130f
mdioumassi <mdioumassi@yahoo.fr>
2021-12-31 10:55:59 +0100

    migration database

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Personnel
+# Gestion du personnel
+#GIT
+-> git clone https://github.com/mdioumassi/Personnel.git
+#Composer
+-> dossier du projet/ composer install
+
+#Database
+taper la commande: composer dump-env prod
+pour créer un env de production: .env.local.php
+ -> Crée la base de données:
+-> p
--- migrations/Version20211231095454.php
+++ migrations/Version20211231095454.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211230171312 extends AbstractMigration
+final class Version20211231095454 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,7 +20,7 @@ final class Version20211230171312 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE persons DROP FOREIGN KEY FK_A25CC7D3FE812AD');
+        $this->addSql('CREATE TABLE persons (id INT AUTO_INCREMENT NOT NULL, persons_id INT DEFAULT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, birthday DATE NOT NULL, address VARCHAR(255) DEFAULT NULL, post VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', photo VARCHAR(255) DEFAULT NULL, INDEX IDX_A25CC7D3FE812AD (persons_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE persons ADD CONSTRAINT FK_A25CC7D3FE812AD FOREIGN KEY (persons_id) REFERENCES persons (id) ON DELETE CASCADE');
     }
 
@@ -28,6 +28,6 @@ final class Version20211230171312 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE persons DROP FOREIGN KEY FK_A25CC7D3FE812AD');
-        $this->addSql('ALTER TABLE persons ADD CONSTRAINT FK_A25CC7D3FE812AD FOREIGN KEY (persons_id) REFERENCES persons (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('DROP TABLE persons');
     }
 }
